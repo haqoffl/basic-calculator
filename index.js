b1e24eeb61a7a002fcd123819972c25766bf36be
haq2005 <91720352+haq2005@users.noreply.github.com>
2022-04-07 21:22:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let screen = document.getElementById('screen');
+function calculator(num){
+  
+screen.value += num;
+
+}
+function reset(){
+document.getElementById('screen').value = "";
+}
+
+function evaluator(){
+  if(screen.value === ""){
+    alert('please fill something')
+  }
+  else{
+    let evaluates = eval(screen.value)
+ screen.value = evaluates;
+  }
+  
+}
